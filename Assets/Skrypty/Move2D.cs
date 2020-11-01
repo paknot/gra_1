7bac96aa57f4e379cf874664ed46c5614b96b78a
paknot <73398335+paknot@users.noreply.github.com>
2020-11-01 21:21:35 +0100

    chod

---
+++ Assets/Skrypty/Move2D.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move2D : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public bool isGrounded = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    Jump();
+    Vector3 poruszanie = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+    transform.position += poruszanie * Time.deltaTime * moveSpeed;
+    }
+    void Jump(){
+        if (Input.GetButtonDown("Jump") && isGrounded == true){
+        gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f,10f), ForceMode2D.Impulse);
+    }
+    }
+}
