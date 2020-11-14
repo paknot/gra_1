d3d0644347bbb5115044f5539b1baca4bef6f5da
paknot <73398335+paknot@users.noreply.github.com>
2020-11-14 19:37:10 +0100

    obrot

---
+++ Assets/Skrypty/Lewo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lewo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    if(Input.GetKeyDown(KeyCode.A)) { //obraca sie jezeli A
+        transform.rotation = Quaternion.Euler(transform.rotation.x, 180, transform.rotation.z);
+     }else if(Input.GetKeyDown(KeyCode.D)) { // obraca sie jezeli D
+       transform.rotation = Quaternion.Euler(transform.rotation.x, 0, transform.rotation.z);
+    }
+    }
+}
